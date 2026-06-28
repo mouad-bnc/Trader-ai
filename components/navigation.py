620f0588fd3e54852a2d2add6c4adc040933fd5d
mouad-bnc <mouadsaissi@me.com>
2026-06-28 13:56:13 +0100

    Fix Streamlit navigation

--- components/navigation.py
+++ components/navigation.py
@@ -3,7 +3,27 @@ from __future__ import annotations
 import streamlit as st
 from utils.constants import NAV_ITEMS
 
+NAV_STATE_KEY = "selected_page"
+
 
 def selected_page() -> str:
-    query = st.query_params.get("page", NAV_ITEMS[0])
-    return query if query in NAV_ITEMS else NAV_ITEMS[0]
+    """Render the top navigation and return the selected page."""
+    current_page = st.session_state.get(NAV_STATE_KEY, NAV_ITEMS[0])
+    if current_page not in NAV_ITEMS:
+        current_page = NAV_ITEMS[0]
+        st.session_state[NAV_STATE_KEY] = current_page
+
+    selected = st.segmented_control(
+        "Navigation",
+        NAV_ITEMS,
+        selection_mode="single",
+        default=current_page,
+        key=NAV_STATE_KEY,
+        label_visibility="collapsed",
+    )
+
+    if selected not in NAV_ITEMS:
+        st.session_state[NAV_STATE_KEY] = NAV_ITEMS[0]
+        return NAV_ITEMS[0]
+
+    return selected
